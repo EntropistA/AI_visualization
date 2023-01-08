59b1bf6adcc7bf2e5d30ab5761ae9093cc783def
Adam Wilczyński <adam.wilcz7@gmail.com>
2023-01-08 23:07:47 +0100

    Clustering with non-working update

--- datasets/datasets.py
+++ datasets/datasets.py
@@ -17,21 +17,24 @@ def project_root():
     os.chdir(previous_cwd)
 
 
-def countries_and_gdp(year=2019):
+def k_to_thousand(number: str) -> float:
+    return float(number.rstrip("k")) * 1000 if number.endswith("k") else number
+
+
+
+def countries_and_gdp(year=2019, normalize=True):
     columns = ["country", str(year)]
 
     income_file_location = Path("datasets", "income_per_person_gdppercapita_ppp_inflation_adjusted.csv")
     with project_root():
         income = pd.read_csv(income_file_location, usecols=columns)
     income.rename(columns={str(year): "gdp"}, inplace=True)
-    income["gdp"] = income["gdp"].str.strip("k")
-    income["gdp"] = income["gdp"].astype("float") * 1_000
+    income["gdp"] = income["gdp"].apply(k_to_thousand)
 
     life_expectancy_file_location = Path("datasets", "life_expectancy_years.csv")
     with project_root():
         life_expectancy = pd.read_csv(life_expectancy_file_location, usecols=columns)
         life_expectancy.rename(columns={"2019": "life_expectancy_years"}, inplace=True)
 
-    income_life_expectancy = income.set_index("country").join(life_expectancy.set_index("country"))
+    income_life_expectancy = income.set_index("country").join(life_expectancy.set_index("country")).astype("float")
     return income_life_expectancy
-
--- pages/1_Clustering.py
+++ pages/1_Clustering.py
@@ -8,28 +8,70 @@ import streamlit as st
 from utils import distance
 from datasets import datasets
 
-st.title("K-Means")
 
-k = st.slider("K")
+def min_max_normalizer(column: pd.Series):
+    return (column - column.min()) / (column.max() - column.min())
+
+
+def normalize_across_column(df: pd.DataFrame):
+    df = df.copy()
+    for column in df.columns:
+        df[column] = min_max_normalizer(df[column])
+    return df
 
 
 class KMeans:
-    def calculate_distance_list_from_centroid_list(self, series: pd.Series) -> dict:
-        return {centroid: distance.euclidean(centroid, series) for _, centroid in self.centroid_df.iterrows()}
-
-    def create_random_centroid(self):
-        self.centroid_df = self.items_df.sample(n=k).reset_index()
+    def set_centroid_to_random_data_point(self) -> None:
+        print("set centroid to random")
+        self.centroid_df = None
+        self.centroid_df = self.data_point_normalized_df.sample(n=self.k).reset_index(drop=True)
         self.centroid_df.index.names = ["centroid"]
 
+    def set_distance_from_data_point_to_centroid(self) -> None:
+        print("set distance")
+        for centroid_index in self.centroid_df.index:
+            self.data_point_df[centroid_index] = None
+
+        for data_point_index, data_point in self.data_point_df.copy().iterrows():
+            for centroid_index, centroid in self.centroid_df.iterrows():
+                distance_between = distance.euclidean(self.data_point_normalized_df.loc[data_point_index], centroid)
+                self.data_point_df.at[data_point_index, centroid_index] = distance_between
+
+    def assign_data_point_to_centroid(self) -> None:
+        print("assign")
+        assert isinstance(self.centroid_df, pd.DataFrame) and not self.centroid_df.empty
+
+        self.data_point_df["assignment"] = None
+        for i, row in self.data_point_df.copy().iterrows():
+            distance_to_centroid = dict(row[self.centroid_df.index])
+            centroid_with_min_distance_index = min(distance_to_centroid.items(), key=lambda x: x[1])[0]
+            self.data_point_df.at[i, "assignment"] = centroid_with_min_distance_index
+
     def update_centroid(self):
+        print("update")
+        for centroid_index, centroid in self.centroid_df.copy().iterrows():
+            assigned_to_centroid = self.data_point_df["assignment"] == centroid_index
+            for attribute in centroid.index:
+                mean = self.data_point_normalized_df[assigned_to_centroid][attribute].mean()
+                print("mean", mean)
+                self.centroid_df.at[centroid_index, attribute] = mean
+        self.assign_data_point_to_centroid()
 
+    def __init__(self, data_point_df: pd.DataFrame, k: int, init="random"):
+        self.data_point_df = data_point_df
+        self.k = k
 
-    def __init__(self, input_df: pd.DataFrame, k: int, init="random"):
-        self.items_df = input_df
+        self.data_point_normalized_df = normalize_across_column(data_point_df)
 
         self.centroid_df = None
-        self.create_random_centroid()
+        self.set_centroid_to_random_data_point()
 
+        self.set_distance_from_data_point_to_centroid()
+
+        self.assign_data_point_to_centroid()
+        # print(self.data_point_df.head())
+
+        # print(self.centroid_df)
         # if init == "random":
         #     self.random_centroid_assignment()
         # else:
@@ -39,15 +81,37 @@ class KMeans:
         #         attribute_column = df[attribute]
         #         attribute_name_min_and_max_value[attribute] = (attribute_column.min(), attribute_column.max())
 
-        self.items_df = input_df
-        input_df["assignment"] = None
-        for i, row in input_df.iterrows():
-            distance_from_centroid = self.calculate_distance_list_from_centroid_list(row)
-            input_df.iloc[i]["assignment"] = min(iterrows.items(), key=lambda name_distance: name_distance[1])
-
-    def visualize(self):
+    def visualize(self) -> plt.Figure:
         fig, ax = plt.subplots()
-        sns.scatterplot(data=self.items_df, ax=ax)
+        sns.scatterplot(data=self.data_point_df, x="gdp", y="life_expectancy_years", hue="assignment", ax=ax)
+        plt.xscale("log")
+        numbers_range = [1000 * 2 ** i for i in range(0, 8)]
+        plt.xticks(numbers_range, numbers_range)
+        return fig
 
 
-k_means_test = KMeans(datasets.countries_and_gdp(), k=5)
+TITLE = "K-Means"
+st.set_page_config(layout="wide", page_title=TITLE)
+
+st.title(TITLE)
+
+configuration = st.container()
+
+k = configuration.slider("K", min_value=1, max_value=10)
+if "k_means" not in st.session_state or st.session_state.k != k:
+    st.session_state.k_means = KMeans(datasets.countries_and_gdp(), k=k)
+    st.session_state.k = k
+
+if configuration.button("Update Centroid"):
+    st.session_state.k_means.update_centroid()
+
+data_view_col, visualization = st.columns(2)
+visualization.pyplot(st.session_state.k_means.visualize())
+
+data_view_container = data_view_col.container()
+
+data_view_container.header("Data Points")
+data_view_container.dataframe(st.session_state.k_means.data_point_df)
+
+data_view_container.header("Centroid")
+data_view_container.dataframe(st.session_state.k_means.centroid_df)
