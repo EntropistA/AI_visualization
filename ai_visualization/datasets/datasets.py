76f4a2130fb9774b3ec93ef81dffd8316ea8e4e9
Adam Wilczyński <adam.wilcz7@gmail.com>
2023-01-09 22:07:23 +0100

    Debug Streamlit Cloud

--- ai_visualization/datasets/datasets.py
+++ ai_visualization/datasets/datasets.py
@@ -35,7 +35,7 @@ def countries_and_gdp(year=2019, normalize=True):
 
     income_file_location = Path("datasets", "income_per_person_gdppercapita_ppp_inflation_adjusted.csv")
     with project_root():
-        raise ValueError(Path.cwd())
+        raise ValueError(Path.cwd(), income_file_location, income_file_location.exists())
         income = pd.read_csv(income_file_location, usecols=columns)
     income.rename(columns={str(year): "gdp"}, inplace=True)
     income["gdp"] = income["gdp"].apply(k_to_thousand)
